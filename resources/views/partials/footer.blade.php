45353bb1c7664b81ab097629cc547176af9f2cdc
ivan3455 <trunovivan641@gmail.com>
2025-10-27 17:14:24 +0200

    Initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,20 @@
+<footer>
+        <div class="footer-personal">
+            <p>ПІБ: <strong>Трунов І. О.</strong></p>
+            <p>Група: <strong>409</strong></p>
+            <p>Варіант: <strong>№ 21</strong></p>
+        </div>
+
+        <div class="footer-tech">
+            <p>Сайт побудовано на технологіях:</p>
+            <div class="tech-logos">
+                <a href="https://laravel.com/" target="_blank" title="Laravel Framework">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg" alt="Laravel Logo">
+                </a>
+
+                <a href="https://www.php.net/" target="_blank" title="PHP">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg" alt="PHP Logo">
+                </a>
+            </div>
+        </div>
+</footer>
